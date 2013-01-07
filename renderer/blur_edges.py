dcdb4d0e74bd44b3fc404a0d2fb04edb1e7af77b
Alex Wiltschko <alex.bw@gmail.com>
2013-01-07 15:17:43 -0500

    Blur edges changes (again)

--- renderer/blur_edges.py
+++ renderer/blur_edges.py
@@ -9,8 +9,8 @@ num_iterations_dilation = 1
 num_iterations_erosion = 3
 blur_sigma = (2,2)
 
-source_behavior_data = "Test Data/Mouse No Median Filter, No Dilation"
-dest_behavior_data = "/Users/Alex/Dropbox/Science/Datta lab/Posture Tracking/Test Data/Blurred Edge 2"
+source_behavior_data = "../Test Data/Mouse No Median Filter, No Dilation"
+dest_behavior_data = "../Test Data/Blurred Edge 2"
 
 path_to_behavior_data = join(dirname(__file__),'..',source_behavior_data)
 
@@ -28,7 +28,4 @@ for i,img in enumerate(images):
 	img[mask] = blurred_img[mask]
 	out_images[i] = img
 
-img_path = "test.int16binary"
-image_height = 80
-image_width = 80
 out_images.tofile(join(dest_behavior_data, "Extracted Mouse Images.int16binary"))
