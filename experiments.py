c8d748db02a4900d4219e0bcc53a633bb7d9eb8a
Matthew Johnson <mattjj@csail.mit.edu>
2013-01-08 01:13:37 -0500

    tweaked params

--- experiments.py
+++ experiments.py
@@ -189,10 +189,10 @@ class RandomWalkLearnedNoise(Experiment):
         cutoff = 1024*15
 
         initial_n_0 = 1000 # TODO could also just switch particle classes
-        subsequent_n_0 = 50
+        subsequent_n_0 = 16+20
 
         initial_randomwalk_noisecov = initial_n_0*np.diag((3.,3.,7.,3.,0.01,2.,2.,10.,) + (20.,)*(2+2*3))**2
-        subsequent_randomwalk_noisecov = subsequent_n_0*np.diag((3.,3.,5.,0.5,0.01,0.1,0.1,0.5,) + (5.,)*(2+2*3))**2
+        subsequent_randomwalk_noisecov = subsequent_n_0*np.diag((1.5,1.5,3.,0.5,0.01,0.05,0.05,0.5,) + (5.,)*(2+2*3))**2
 
         pose_model = pose_models.PoseModel3()
 
