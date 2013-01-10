3a7b16681bf9821deb6f38a9cf1c9834d81b2471
Matthew Johnson <mattjj@csail.mit.edu>
2013-01-09 20:31:27 -0500

    fixed some errors

--- parallel.py
+++ parallel.py
@@ -8,13 +8,15 @@ def go_parallel(scenefile_path_relative_to_engines,behavior_data_path_relative_t
         c = Client()
         dv = c[:]
 
-        dv.execute(
-        '''
+        codestr = \
+'''
 import experiments
-ms = experiments._build_mousescene(%(scenefile)s)
-images, xytheta = experiments._load_data(%(datapath)s,%(frame_range)s)
-''' % {'scenefile':scenefile_path_relative_to_engines,'datapath':behavior_data_path_relative_to_engines,'frame_range':frame_range},
-        block=True)
+ms = experiments._build_mousescene("%(scenefile)s")
+images, xytheta = experiments._load_data("%(datapath)s",%(frame_range)s)
+''' % {'scenefile':scenefile_path_relative_to_engines,'datapath':behavior_data_path_relative_to_engines,'frame_range':frame_range}
+
+        print codestr
+        dv.execute(codestr,block=True)
 
     return dv
 
