7dae3da661b7f131635fde82ec07ca0be1536990
Alex Wiltschko <alex.bw@gmail.com>
2013-01-30 20:02:16 -0500

    Script for extracting the indices of selected vertices

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -758,7 +758,7 @@ def test_single_mouse():
     image = load_behavior_data(path_to_behavior_data, which_img+1, 'images')[-1]
     image = image.T[::-1,:].astype('float32')
 
-    num_particles = 64**2
+    num_particles = 128**2
     numCols = 32
     numRows = 32
     scenefile = os.path.join(os.path.dirname(__file__),"data/mouse_mesh_low_poly3.npz")
